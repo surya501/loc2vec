e9e1775b557c19712b2254e29ee52197c8caf285
Suresh Rangarajulu <surya501@yahoo.com>
2019-02-27 12:58:41 -0800

    removed siamese network and added EmbeddingL2Net

--- networks.py
+++ networks.py
@@ -33,44 +33,13 @@ class EmbeddingNetL2(EmbeddingNet):
 
     def forward(self, x):
         output = super(EmbeddingNetL2, self).forward(x)
-        output /= output.pow(2).sum(1, keepdim=True).sqrt()
+        # output /= (output.pow(2).sum(1, keepdim=True).sqrt())
+        # fix for pytorch 0.4 -> 1.0
+        output = output/(output.pow(2).sum(1, keepdim=True).sqrt())
         return output
 
     def get_embedding(self, x):
         return self.forward(x)
-
-
-class ClassificationNet(nn.Module):
-    def __init__(self, embedding_net, n_classes):
-        super(ClassificationNet, self).__init__()
-        self.embedding_net = embedding_net
-        self.n_classes = n_classes
-        self.nonlinear = nn.PReLU()
-        self.fc1 = nn.Linear(2, n_classes)
-
-    def forward(self, x):
-        output = self.embedding_net(x)
-        output = self.nonlinear(output)
-        scores = F.log_softmax(self.fc1(output), dim=-1)
-        return scores
-
-    def get_embedding(self, x):
-        return self.nonlinear(self.embedding_net(x))
-
-
-class SiameseNet(nn.Module):
-    def __init__(self, embedding_net):
-        super(SiameseNet, self).__init__()
-        self.embedding_net = embedding_net
-
-    def forward(self, x1, x2):
-        output1 = self.embedding_net(x1)
-        output2 = self.embedding_net(x2)
-        return output1, output2
-
-    def get_embedding(self, x):
-        return self.embedding_net(x)
-
 
 class TripletNet(nn.Module):
     def __init__(self, embedding_net):
